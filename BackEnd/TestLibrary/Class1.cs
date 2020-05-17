897a8c7166449b6bd3618a42aa23ef57d8058995
John Pankowicz <johnpankowicz@hotmail.com>
2020-05-17 16:00:20 -0400

    Copied TestLibrary to Backend. Did not work. Remove and re-add WorkflowApp2 & it worked

---
+++ BackEnd/TestLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestLibrary
+{
+    public static class Class1
+    {
+        public static void DoSomething()
+        {
+            Console.WriteLine("I did something");
+        }
+    }
+}
