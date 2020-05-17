94223a8f3f87f6493ccf517d1a4da3183b1a6830
John Pankowicz <johnpankowicz@hotmail.com>
2020-05-17 11:44:45 -0400

    Add WorkflowApp2 test project. Add Nuget package references.

---
+++ BackEnd/WorkflowApp2/Program2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//using GM.Utilities;
+
+namespace WorkflowApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //string secrets = GMFileAccess.FindParentFolderWithName("SECRETS");
+            string credentialsFilePath = @"C:\GOVMEETING\SECRETS\TranscribeAudio.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", credentialsFilePath);
+
+            //StorageClient storageClient = StorageClient.Create();
+        }
+    }
+}
