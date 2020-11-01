f53a901dda6c28ca4ae92dea5f1f87798a21926b
John Pankowicz <johnpankowicz@hotmail.com>
2020-11-01 17:17:23 -0500

    update startup.cs. program.cs &other server files

--- src/WebUI/WebApp/Program.cs
+++ src/WebUI/WebApp/Program.cs
@@ -1,59 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using NLog.Web;
-using GM.FileDataRepositories;
-using GM.Utilities;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-
-namespace GM.WebApp
+namespace sample
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        // CreateDefaultBuilder would normally add both appsettings.json and the appsettings for our current environment.
-        // But our appsettings.Development.json is in the secrets folder, so we need to specifically add it here.
-        // When we deploy to production, we upload appsettings.Production.json from the secrets folder.
-        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>()
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                //.UseStartup<Startup>()
-                //.ConfigureAppConfiguration((hostingContext, config) =>
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
-                var env = hostingContext.HostingEnvironment;
-                if (env.IsDevelopment())
-                {
-                    // If development, include SECRETS/appsettings.Development.json in the configuration.
-                    // This file contains the keys for using reCaptcha and Google external authorization.
-                    string secretsFolder = GMFileAccess.GetSolutionSiblingFolder("SECRETS");
-                    string devSettingFile = secretsFolder + "/" + $"appsettings.{env.EnvironmentName}.json";
-                    if (File.Exists(devSettingFile))
-                    {
-                        config.AddJsonFile(devSettingFile, optional: true, reloadOnChange: true);
-                    }
-                }
-            })
-            .ConfigureLogging(logging =>
-            {
-                logging.ClearProviders();
-                logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-            })
-            .UseNLog();  // NLog: setup NLog for Dependency injection
-        });
-
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
