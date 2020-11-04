8e5b35d4c0556d8a74e375b6c4d6b3bd881f1392
John Pankowicz <johnpankowicz@hotmail.com>
2020-11-04 17:18:47 -0500

    Attempt to use IHostBuilder. Err on UseNLog

--- src/WebUI/WebAppNew/Program.cs
+++ src/WebUI/WebAppNew/Program.cs
@@ -1,11 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
+using GM.FileDataRepositories;
+using GM.Utilities;
+using Microsoft.Extensions.Hosting;
+
 
 namespace WebAppNew
 {
@@ -16,11 +22,66 @@ namespace WebAppNew
             CreateHostBuilder(args).Build().Run();
         }
 
+        // CreateDefaultBuilder would normally add both appsettings.json and the appsettings for our current environment.
+        // But our appsettings.Development.json is in the secrets folder, so we need to specifically add it here.
+        // When we deploy to production, we upload appsettings.Production.json from the secrets folder.
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                //.UseStartup<Startup>()
+                //.ConfigureAppConfiguration((hostingContext, config) =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    var env = hostingContext.HostingEnvironment;
+                    if (env.IsDevelopment())
+                    {
+                        // If development, include SECRETS/appsettings.Development.json in the configuration.
+                        // This file contains the keys for using reCaptcha and Google external authorization.
+                        string secretsFolder = GMFileAccess.GetSolutionSiblingFolder("SECRETS");
+                        string devSettingFile = secretsFolder + "/" + $"appsettings.{env.EnvironmentName}.json";
+                        if (File.Exists(devSettingFile))
+                        {
+                            config.AddJsonFile(devSettingFile, optional: true, reloadOnChange: true);
+                        }
+                    }
+                })
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+            })
+            .UseNLog();  // NLog: setup NLog for Dependency injection
+            });
+
     }
 }
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Logging;
+
+//namespace WebAppNew
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            CreateHostBuilder(args).Build().Run();
+//        }
+
+//        public static IHostBuilder CreateHostBuilder(string[] args) =>
+//            Host.CreateDefaultBuilder(args)
+//                .ConfigureWebHostDefaults(webBuilder =>
+//                {
+//                    webBuilder.UseStartup<Startup>();
+//                });
+//    }
+//}
