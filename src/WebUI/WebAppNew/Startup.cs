25c2f1f06103c0c0698ed6d0b6faee6be070e00b
John Pankowicz <johnpankowicz@hotmail.com>
2020-11-06 11:28:45 -0500

    add from nlog to features to Startup.Configure

--- src/WebUI/WebAppNew/Startup.cs
+++ src/WebUI/WebAppNew/Startup.cs
@@ -1,22 +1,28 @@
+using GM.Configuration;
+using GM.DatabaseAccess;
 using GM.Utilities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NLog;
+using GM.WebApp.StartupCustomizations;
 
 namespace WebAppNew
 {
-    public class Startup
+    public partial class Startup
     {
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        NLog.Logger logger;
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -29,7 +35,57 @@ namespace WebAppNew
             //string logfilesPath = GMFileAccess.GetFullPath(Configuration["AppSettings:LogfilesPath"]);
             GlobalDiagnosticsContext.Set("logfilesPath", logfilesPath);
 
+            //####################################
+            // Create an instance of NLog.Logger manually here since it is not available
+            // from dependency injection yet.
+            logger = LogManager.LoadConfiguration("nlog.config").GetCurrentClassLogger();
+            logger.Info("Just set value in GDC for NLog and created NLog.Logger instance");
+
+            //####################################
+            logger.Info("Configure AppSettings options");
+            services.AddOptions();
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+            services.Configure<AppSettings>(myOptions =>
+            {
+                logger.Info("Modify the configuration path options to be full paths.");
+                // Modify the configuration path options to be full paths.
+                myOptions.DatafilesPath = GMFileAccess.GetSolutionSiblingFolder(myOptions.DatafilesPath);
+                myOptions.TestdataPath = GMFileAccess.GetSolutionSiblingFolder(myOptions.TestdataPath);
+                logger.Info("DatafilesPath: {0}, TestdataPath: {2}",
+                    myOptions.DatafilesPath, myOptions.TestdataPath);
+            });
+
+            //####################################
+            logger.Info("Add ApplicationDbContext");
+            services.AddTransient<DBOperations>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration["AppSettings:ConnectionString"]
+                    //sqlServerOptions => sqlServerOptions.MigrationsAssembly("DatabaseAccess_Lib")
+                    //sqlServerOptions => sqlServerOptions.MigrationsAssembly("WebApp")
+                    ));
+
+            /////// TODO - uncomment
+            //####################################
+            logger.Info("Add Add Authentication");
+            // ConfigureAuthenticationServices(services, logger);
+
+
+            //####################################
+            // Add the MVC Controller services that are common to both Web API and MVC,
+            // and also add the services required for rendering Razor views. 
+            logger.Info("Add Controllers with Views");
             services.AddControllersWithViews();
+
+
+            //####################################
+            logger.Info("Enable Feature Folders");
+            // https://scottsauber.com/2016/04/25/feature-folder-structure-in-asp-net-core/
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.ViewLocationExpanders.Add(new FeatureLocationExpander());
+            });
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
