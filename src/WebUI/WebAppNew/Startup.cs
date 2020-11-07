e84e63bc1e657fe220a4eee782b5ebde6342bbd3
John Pankowicz <johnpankowicz@hotmail.com>
2020-11-07 18:54:29 -0500

    Complete auth cleanup and organization

--- src/WebUI/WebAppNew/Startup.cs
+++ src/WebUI/WebAppNew/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using GM.Configuration;
 using GM.DatabaseAccess;
 using GM.Utilities;
@@ -15,6 +16,9 @@ using GM.WebApp.StartupCustomizations;
 using System.Net.Security;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Identity;
 
 namespace WebAppNew
 {
@@ -31,19 +35,27 @@ namespace WebAppNew
         public void ConfigureServices(IServiceCollection services)
         {
             ConfigureLoggingService();
-            logger.Info("ConfigureLoggingService");
+            logger.Info("Configure Logging Service");
 
             ConfigureAppsettings(services);
-            logger.Info("ConfigureAppsettings");
+            logger.Info("Configure Appsettings");
 
             ConfigureDbContext(services);
-            logger.Info("ConfigureDbContext");
+            logger.Info("Configure DbContext");
 
             services.AddHealthChecks();
             logger.Info("AddHealthChecks");
 
-            ConfigureAuthenticationServices(services, logger);
-            logger.Info("Add Add Authentication");
+            ConfigureAuthentication(services);
+            logger.Info("Configure Authentication");
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            ConfigureIdentity(services);
+            logger.Info("Configure Identity");
+
+            ConfigureAuthorization(services);
+            logger.Info("Add Authorization");
 
             services.AddControllersWithViews();
             logger.Info("Add services for Web API, MVC & Razor Views");
@@ -89,19 +101,7 @@ namespace WebAppNew
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.Use(next => context =>
-            {
-                var endpoint = context.GetEndpoint();
-                if (endpoint == null)
-                {
-                    return next(context);
-                }
-
-                var route = (endpoint is RouteEndpoint routeEndpoint) ? routeEndpoint.RoutePattern : null;
-                var metadata = endpoint.Metadata;
-
-                return next(context);
-            });
+            DebugCurrentEndpoint(app);
 
             // END OF ENDPOINT ZONE
 
@@ -168,5 +168,106 @@ namespace WebAppNew
                     //sqlServerOptions => sqlServerOptions.MigrationsAssembly("WebApp")
                     ));
         }
+
+        private void ConfigureAuthentication(IServiceCollection services)
+        {
+            services.AddAuthentication()
+            //.AddGoogle(options =>
+            //{
+            //    options.ClientId = Configuration["ExternalAuth:Google:ClientId"];
+            //    options.ClientSecret = Configuration["ExternalAuth:Google:ClientSecret"];
+            //});
+            // https://docs.microsoft.com/en-us/dotnet/core/compatibility/2.2-3.1#authentication-google-deprecated-and-replaced
+            .AddOpenIdConnect("Google", o =>
+            {
+                o.ClientId = Configuration["ExternalAuth:Google:ClientId"];
+                o.ClientSecret = Configuration["ExternalAuth:Google:ClientSecret"];
+                o.Authority = "https://accounts.google.com";
+                o.ResponseType = OpenIdConnectResponseType.Code;
+                o.CallbackPath = "/signin-google"; // Or register the default "/sigin-oidc"
+                o.Scope.Add("email");
+            });
+        }
+
+        private void ConfigureIdentity(IServiceCollection services)
+        {
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                // TODO - While upgrading to .NET SDK 2.0, I was getting an error seting these options, so
+                // I commented them out. Error = "IdentityOptions does not contain a definition for Cookies"
+                // Govmeeting: Set options for cookie expiration.
+                //options.Cookies.ApplicationCookie.SlidingExpiration = true;
+                //options.Cookies.ApplicationCookie.ExpireTimeSpan = TimeSpan.FromHours(1);
+
+                options.Password.RequiredLength = 8;
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); // amount of time they are locked out
+                options.Lockout.AllowedForNewUsers = true;
+                // TODO We should send the admin an email if someone is locked out.
+                options.SignIn.RequireConfirmedEmail = true;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+            //// Govmeeting: Brock Allen suggest stronger hashing instead of the default.
+            ////services.Configure<PasswordHasherOptions>(options =>
+            ////{
+            ////    options.CompatibilityMode = PasswordHasherCompatibilityMode.IdentityV2;
+            ////    options.IterationCount = 20000;
+            ////});
+        }
+
+        private void ConfigureAuthorization(IServiceCollection services)
+        {
+            // https://docs.asp.net/en/latest/security/authorization/claims.html
+            services.AddAuthorization(options =>
+            {
+                // In DbInitializer, the admin user and administrator role is created.
+                // The password and email is read from appsettings.
+                // The methods in Features/Admin/AdminController.cs require: Policy = "Administrator" 
+                options.AddPolicy("Administrator", policy =>
+                { policy.RequireClaim("role", "administrator"); });
+
+                options.AddPolicy("Editor", policy =>
+                { policy.RequireClaim("role", "editor"); });
+
+                options.AddPolicy("Proofreader", policy =>
+                { policy.RequireClaim("role", "proofreader"); });
+
+                // Add some sample claims for our test data
+
+                options.AddPolicy("PhillyEditor", policy =>
+                {
+                    policy.RequireClaim("role", "editor");
+                    policy.RequireClaim("location", "Philadelphia");
+                });
+                options.AddPolicy("BbhEditor", policy =>
+                {
+                    policy.RequireClaim("role", "editor");
+                    policy.RequireClaim("location", "Boothbay Harbor");
+                });
+            });
+        }
+
+
+        // Examine contents of endpoint for current request (for debugging)
+        private void DebugCurrentEndpoint(IApplicationBuilder app)
+        {
+            app.Use(next => context =>
+            {
+                var endpoint = context.GetEndpoint();
+                if (endpoint == null)
+                {
+                    return next(context);
+                }
+
+                var route = (endpoint is RouteEndpoint routeEndpoint) ? routeEndpoint.RoutePattern : null;
+                var metadata = endpoint.Metadata;
+
+                return next(context);
+            });
+        }
+
+
     }
 }
