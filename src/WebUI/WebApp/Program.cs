c5580eb50e811cfd99bdcfbda928c376359e02b7
John Pankowicz <johnpankowicz@hotmail.com>
2020-11-07 19:28:09 -0500

    Copy features & services folders from old. Replace WebAppwith WebAppNew.

--- src/WebUI/WebApp/Program.cs
+++ src/WebUI/WebApp/Program.cs
@@ -1,13 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using NLog.Web;
+using GM.Utilities;
 
-namespace sample
+
+namespace GM.WebApp
 {
     public class Program
     {
@@ -16,11 +17,36 @@ namespace sample
             CreateHostBuilder(args).Build().Run();
         }
 
+        // CreateDefaultBuilder would normally add both appsettings.json and the appsettings for our current environment.
+        // But our appsettings.Development.json is in the secrets folder, so we need to specifically add it here.
+        // When we deploy to production, we upload appsettings.Production.json from the secrets folder.
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>()
+                .ConfigureAppConfiguration((hostingContext, config) =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    var env = hostingContext.HostingEnvironment;
+                    if (env.IsDevelopment())
+                    {
+                        // If development, include SECRETS/appsettings.Development.json in the configuration.
+                        // This file contains the keys for using reCaptcha and Google external authorization.
+                        string secretsFolder = GMFileAccess.GetSolutionSiblingFolder("SECRETS");
+                        string devSettingFile = secretsFolder + "/" + $"appsettings.{env.EnvironmentName}.json";
+                        if (File.Exists(devSettingFile))
+                        {
+                            config.AddJsonFile(devSettingFile, optional: true, reloadOnChange: true);
+                        }
+                    }
+                })
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+            })
+            .UseNLog();  // NLog: setup NLog for Dependency injection
+            });
+
     }
 }
--- src/WebUI/WebAppNew/ClientApp/src/app/appdata.ts
+++
@@ -1,6 +0,0 @@
-export class AppData {
-  public isAspServerRunning: boolean;
-  public isBeta: boolean;
-  public isLargeEditData: boolean;
-  // public isDataFromMemory: boolean;
-}
